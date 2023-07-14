e47db126173d3de0bf545e738dd5e2d329f681b6
kunalb20 <136967201+kunalb20@users.noreply.github.com>
2023-07-14 17:14:37 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -8,4 +8,6 @@ echo "Hello All, Welcome to AWS/Devops Class"
 echo "Welcome to Young Minds"
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello batch-19"
-
+awj dniuadk  dnsod SD AS
+D ASFAS;FL KMKDM JNK amdm sdslnf da
+asd;f joasj d,njnnasdnn knnks dasd
